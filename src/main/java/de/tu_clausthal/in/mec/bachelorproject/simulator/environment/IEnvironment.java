a36a80ca02eed31c427408c1e93584b41d1eeb6a
Awad Mukbil <awad.mukbil@tu-clausthal.de>
2020-02-20 18:16:34 +0100

    environment is implemented

--- src/main/java/de/tu_clausthal/in/mec/bachelorproject/simulator/environment/IEnvironment.java
+++ src/main/java/de/tu_clausthal/in/mec/bachelorproject/simulator/environment/IEnvironment.java
@@ -1,4 +1,21 @@
 package de.tu_clausthal.in.mec.bachelorproject.simulator.environment;
 
+import cern.colt.matrix.tdouble.DoubleMatrix1D;
+import de.tu_clausthal.in.mec.bachelorproject.simulator.entities.IEntity;
+
+import java.util.List;
+
 public interface IEnvironment {
+
+    void put(List<IEntity> p_entitis);
+
+    void execute();
+
+    void remove(IEntity p_entity);
+
+    IEntity move(IEntity p_entity, DoubleMatrix1D p_position);
+
+    List<IEntity> entities();
+
+    boolean positionbyondgrid(DoubleMatrix1D p_position);
 }
