c5ccd82a45e14dd5d8710c5a72895b2579ab58b0
Awad Mukbil <awad.mukbil@tu-clausthal.de>
2020-02-20 18:15:16 +0100

    base entity class/interface created

---
+++ src/main/java/de/tu_clausthal/in/mec/bachelorproject/simulator/entities/IBaseEntity.java
@@ -0,0 +1,4 @@
+package de.tu_clausthal.in.mec.bachelorproject.simulator.entities;
+
+public abstract class IBaseEntity implements IEntity {
+}
--- src/main/java/de/tu_clausthal/in/mec/bachelorproject/simulator/entities/IEntity.java
+++ src/main/java/de/tu_clausthal/in/mec/bachelorproject/simulator/entities/IEntity.java
@@ -1,6 +1,19 @@
 package de.tu_clausthal.in.mec.bachelorproject.simulator.entities;
 
+import cern.colt.matrix.tdouble.DoubleMatrix1D;
+import de.tu_clausthal.in.mec.bachelorproject.simulator.common.EDirection;
+
 import java.util.concurrent.Callable;
 
 public interface IEntity extends Callable {
+
+    void speed(double p_speed);
+
+    double speed();
+
+    void position(DoubleMatrix1D p_position);
+
+    DoubleMatrix1D position();
+
+    EDirection direction();
 }
